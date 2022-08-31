c77bf5f4f254488b7595dde26f9c5170cf36c852
David Sargeant <admin@sesaonsite.com>
2022-08-31 18:18:19 -0500

    Moved source to src and built it

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
 'use strict';
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var pouchdbAdapterWebsqlCore = _interopDefault(require('pouchdb-adapter-websql-core'));
-
-var assign;
-
-if (typeof Object.assign === 'function') {
-  assign = Object.assign;
-} else {
-  // lite Object.assign polyfill based on
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-  assign = function (target) {
-    var arguments$1 = arguments;
-
-    var to = Object(target);
-
-    for (var index = 1; index < arguments.length; index++) {
-      var nextSource = arguments$1[index];
-
-      if (nextSource != null) { // Skip over if undefined or null
-        for (var nextKey in nextSource) {
-          // Avoid bugs when hasOwnProperty is shadowed
-          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-            to[nextKey] = nextSource[nextKey];
-          }
-        }
-      }
-    }
-    return to;
-  };
-}
-
-var assign_1 = assign;
+var WebSqlPouchCore = require('pouchdb-adapter-websql-core');
+var assign = require('./assign');
 
 /* global cordova, sqlitePlugin, openDatabase */
 function createOpenDBFunction (opts) {
@@ -43,7 +12,7 @@ function createOpenDBFunction (opts) {
       // It's better to just use their "new" format and pass in a big ol'
       // options object. Also there are many options here that may come from
       // the PouchDB constructor, so we have to grab those.
-      var sqlitePluginOpts = assign_1({}, opts, {
+      var sqlitePluginOpts = assign({}, opts, {
         name: name,
         version: version,
         description: description,
@@ -59,7 +28,7 @@ function createOpenDBFunction (opts) {
 
 function CordovaSQLitePouch (opts, callback) {
   var websql = createOpenDBFunction(opts);
-  var _opts = assign_1({
+  var _opts = assign({
     websql: websql
   }, opts);
 
@@ -72,10 +41,10 @@ function CordovaSQLitePouch (opts, callback) {
       '\n - https://github.com/Microsoft/cordova-plugin-websql');
   }
 
-  if ('default' in pouchdbAdapterWebsqlCore && typeof pouchdbAdapterWebsqlCore.default.call === 'function') {
-    pouchdbAdapterWebsqlCore.default.call(this, _opts, callback);
+  if ('default' in WebSqlPouchCore && typeof WebSqlPouchCore.default.call === 'function') {
+    WebSqlPouchCore.default.call(this, _opts, callback);
   } else {
-    pouchdbAdapterWebsqlCore.call(this, _opts, callback);
+    WebSqlPouchCore.call(this, _opts, callback);
   }
 }
 
@@ -95,6 +64,4 @@ if (typeof window !== 'undefined' && window.PouchDB) {
   window.PouchDB.plugin(cordovaSqlitePlugin);
 }
 
-var src = cordovaSqlitePlugin;
-
-module.exports = src;
+module.exports = cordovaSqlitePlugin;
